fe68411414a487578f38cdcb5e1baa47c1e9c832
Denis19850204 <dg04021985@gmail.com>
2022-11-05 18:05:18 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,7 @@
 import { SecondTitle } from '../SecondTitle/SecondTitle';
 import { StatisticsList } from './StatisticsList';
 import React from 'react';
+// import FeedBack from 'components/FeedBack/FeedBack';
 import FeedBack from 'components/FeedBack/FeedBack';
 
 class Statistics extends React.Component {
